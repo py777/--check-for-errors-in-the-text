b5029781461b2f7b9b7b70ac14e17dc2e665a10c
Dmitry J. Kirzhanov <kirzhanoff@gmail.com>
2015-10-22 17:16:14 +0300

    First commit -- add new methods

--- src/by/javaminsk/service/CheckErrors.java
+++ src/by/javaminsk/service/CheckErrors.java
@@ -18,36 +18,49 @@ public class CheckErrors {
         return outputText.toString();
     }
 
+    //Check wrong symbols in text -- Pegas
     static private StringBuilder checkWrongSymbols(StringBuilder inputText){
         StringBuilder outputText = inputText;
 
         return outputText;
     }
 
+    //Check wrong letters: numbers, gaps etc. -- Anton
     static private StringBuilder checkWrongLetters(StringBuilder inputText){
         StringBuilder outputText = inputText;
 
         return outputText;
     }
 
+    //Check english and russian letters in text -- Ilya
     static private StringBuilder checkUnicode(StringBuilder inputText){
         StringBuilder outputText = inputText;
 
         return outputText;
     }
 
+    //Anton
     static private StringBuilder checkDuplicateLetters(StringBuilder inputText){
         StringBuilder outputText = inputText;
 
         return outputText;
     }
 
+    //The name speaks for itself -- Sasha
     static private StringBuilder checkPedovkaStyle(StringBuilder inputText){
         StringBuilder outputText = inputText;
 
         return outputText;
     }
 
+    //The name speaks for itself -- Ruslan
+    static private StringBuilder addCopyright(StringBuilder inputText){
+        StringBuilder outputText = inputText;
+
+        return outputText;
+    }
+
+    //Pasha
     static public void printText(String inputText){
 
     }
